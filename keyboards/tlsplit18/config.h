2caa7c20f8e463ed9708b75ac4b903bb0f16b83a
satromi <66715943+satromi@users.noreply.github.com>
2022-08-15 22:21:32 +0900

    Add files via upload
    update

--- keyboards/tlsplit18/config.h
+++ keyboards/tlsplit18/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID 0x8774
 #define DEVICE_VER 0x0001
 #define MANUFACTURER satromi
-#define PRODUCT tlsplit18
+#define PRODUCT TL Split Keyboard Keyboard 18mm Rev1
 #define DESCRIPTION A splite keyboard for TRON fan
 
 /* key matrix size */
